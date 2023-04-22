2b261ae36c1e645af798e0f8bd5131dc4066bb1b
Astro175 <aspark400@gmail.com>
2023-04-22 14:51:32 +0100

    added ensure

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Script that creates a file with permissions
 
 file { '/tmp/school':
-  ensure = 'file',
-  mode = '0744',
-  owner = 'www-data',
-  group = 'www-data',
-  content = 'I love Puppet',
+  ensure => 'present',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
