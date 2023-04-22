9ad1f624f606705fe8d9a5af643d26e1a9241c51
Astro175 <aspark400@gmail.com>
2023-04-22 14:53:10 +0100

    fixed linter

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Script that creates a file with permissions
 
 file { '/tmp/school':
-  ensure => 'present',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
