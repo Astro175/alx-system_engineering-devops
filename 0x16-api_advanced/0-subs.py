e3d1d2ee9ab2816275f695fc8bf6b0b5d0ff2e27
astro175 <aspark400@gmail.com>
2023-06-07 01:26:51 +0100

    added edge case for null

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,8 @@ def number_of_subscribers(subreddit):
     """
        Scrapes reddit api and gets the number of subscribers
     """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
     header = {'User-Agent': 'Get subscribers info'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
