671523858222b02edf2027abebd1be981d6a801f
astro175 <aspark400@gmail.com>
2023-06-10 17:09:15 +0100

    added task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that prints title of hot subbreddit"""
+
+import requests
+
+def top_ten(subreddit):
+    """Takes in a subreddit and prints the top hot"""
+    url = f'https://www.reddit.com/r/programming/hot.json'
+    params = {'limit': 10}
+    header = {'User-agent': 'Chrome'}
+    try:
+        response = requests.get(url, headers=header, params=params)
+        response.raise_for_status()
+
+        data = response.json()
+        posts = data["data"]["children"]
+
+        if not posts:
+            print("None")
+        else:
+            for post in posts:
+                title = post["data"]["title"]
+                print(title)
+
+    except requests.exceptions.RequestException:
+        print("None")
